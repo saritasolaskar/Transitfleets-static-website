55221f73fc69bc3c4c2e2de14561ce8c229e230c
sarita <saritasolaskar@gmail.com>
2025-10-04 20:38:47 +0530

    commitd

--- index.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-
-main().then((res)=> console.log('Connected to MongoDB'))
-.catch(err => console.log(err));
-
-async function main() {
-  await mongoose.connect('mongodb://127.0.0.1:27017/test');
-}
